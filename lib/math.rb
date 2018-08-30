dd13fe7b79f318ff1ad7afb1a14fede6b6acb802
Irving E Rabel <rabel606@gmail.com>
2018-08-30 19:39:12 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,18 +1,21 @@
 def addition(num1, num2)
 num1 + num2 
 end
-addition()
+addition(5,4)
 
 def subtraction(num1, num2)
-10-5
+num1 - num2
 end
+subtraction(10,5)
 
 def division(num1, num2)
-50/2
+num1 / num2
 end
+division(50,2)
 
 def multiplication(num1, num2)
 end
+
 
 def modulo(num1, num2)
 end
